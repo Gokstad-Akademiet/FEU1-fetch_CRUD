45540b36ae7d9e9578a0b7e6106e669de322a17a
Egil Skjelbred <eskjelbred@gmail.com>
2023-03-27 13:44:46 +0200

    added CRUS-operations

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ const ownername = document.querySelector("#ownername");
 const species = document.querySelector("#species");
 
 const listPets = document.querySelector("#list-pets");
+const singlePet = document.querySelector("#single-pet");
+const singlePetEdit = document.querySelector("#single-pet-edit");
 
 form.addEventListener("submit", storePet);
 
@@ -47,6 +49,7 @@ async function getPets() {
 	await fetch(`${API_BASE}/pets`)
 		.then((response) => response.json())
 		.then((response) => {
+			listPets.innerHTML = "";
 			response.forEach((pet) => {
 				listPets.innerHTML += `
                 <tr>
@@ -57,6 +60,7 @@ async function getPets() {
                     <td>${pet.species}</td>
                     <td>
                     <p id="edit-btn" onclick="getPetById(${pet.petID})">Get single</p>
+                    <p id="remove-btn" onclick="deletePet(${pet.petID})">Remove</p>
                     </td>
                 </tr>    
                 `;
@@ -70,9 +74,76 @@ getPets();
 async function getPetById(id) {
 	await fetch(`${API_BASE}/pet/${id}`)
 		.then((response) => response.json())
-		.then((response) => console.log(response));
+		.then((response) => {
+			console.log(response);
+			singlePet.innerHTML = `
+            <h3>Navn: ${response[0].petname}</h3>
+            <p>Alder: ${response[0].petage}</p>
+            <p>Eier: ${response[0].ownername}</p>
+            <p>Type: ${response[0].species}</p>
+            <button onclick="editPet(${response[0].petID})">Edit</button>
+            `;
+		});
 }
 
 // UPDATE: Update a pet by id
+async function editPet(id) {
+	await fetch(`${API_BASE}/pet/${id}`)
+		.then((response) => response.json())
+		.then((response) => {
+			singlePetEdit.innerHTML = `
+            <form id="pet-update-form" >
+                <label for="petname">Pet Name:</label>
+                <input type="text" id="petnameEdit" name="petname" value="${response[0].petname}" required />
+                <br />
+                <label for="petage">Pet Age:</label>
+                <input type="number" id="petageEdit" name="petage" value="${response[0].petage}" required />
+                <br />
+                <label for="ownername">Owner Name:</label>
+                <input type="text" id="ownernameEdit" name="ownername" value="${response[0].ownername}" required />
+                <br />
+                <label for="species">Species:</label>
+                <input type="text" id="speciesEdit" name="species" value="${response[0].species}" required />
+                <br />
+                <button onclick="updatePet(event,${response[0].petID})">Update</button>
+            </form>
+
+            `;
+		});
+}
+
+async function updatePet(event, id) {
+	event.preventDefault();
+
+	const petnameEdit = document.querySelector("#petnameEdit");
+	const petageEdit = document.querySelector("#petageEdit");
+	const ownernameEdit = document.querySelector("#ownernameEdit");
+	const speciesEdit = document.querySelector("#speciesEdit");
+
+	let updatedPet = {
+		petname: petnameEdit.value,
+		petage: petageEdit.value,
+		ownername: ownernameEdit.value,
+		species: speciesEdit.value,
+	};
+
+	await fetch(`${API_BASE}/update/${id}`, {
+		method: "PUT",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(updatedPet),
+	});
+
+	// getPets();
+	location.reload();
+}
 
 // DELETE: Delete a pet by id
+async function deletePet(id) {
+	await fetch(`${API_BASE}/pet/${id}`, {
+		method: "DELETE",
+	});
+
+	getPets();
+}
